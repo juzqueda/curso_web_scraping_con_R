1ba3b83baaaa330b159c648eca01fd8884eaf173
juzqueda <juzqueda@mail.utdt.edu>
2025-04-10 01:43:59 -0300

    Update evaluacion.r

--- evaluacion.r
+++ evaluacion.r
@@ -7,26 +7,57 @@ library(rvest)
 
 # Leer la página web
 url <- "https://www.leonainmobiliaria.com.ar/estado/en-alquiler/"
+
 pagina <- read_html(url)
 
-# Seleccionar el contenedor principal
+
+# Se selecciona el contenedor principal de los datos a extraer 
+
 contenedor <- pagina %>%
-  html_nodes(".listing-view.list-view.card-deck")  # Clases separadas por puntos
+  html_nodes(".listing-view.list-view.card-deck")  
 
 contenedor
 
-# Se guardan los titulos o nombre de los alquileres ofrecidos
+print(contenedor)
+
+
+# Se guardan los titulos o nombres de los alquileres ofrecidos
 
 alquileres <- contenedor %>%
-html_nodes(".item-title") 
+  html_nodes(".item-title") 
 
 alquileres
 
 nombre_alq <- alquileres %>%
-  html_text2()  # Extrae el valor del atributo "item-title"
+  html_text2()  
 
 nombre_alq
 
+
+# Se extraén las urls de los alquileres publicados
+
+sub_url_2 <- contenedor %>%
+  html_nodes(".item-title") |>
+  html_element("a") |>
+  html_attr("href")
+
+sub_url_2 
+
+
+
+# Se extraén los precios
+
+precios <- contenedor %>%
+  html_nodes(".item-listing-wrap") %>% 
+  html_node(".item-body .item-price-wrap.hide-on-list .item-price") %>%
+  html_text2()
+
+precios
+
+
+
+
+#######
 
 sub_url <- contenedor %>%
   html_nodes("h2.item-title a") %>%  # Selecciona los elementos <a> dentro de <h2 class="item-title">
